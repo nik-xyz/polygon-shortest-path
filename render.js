4eba6724044849685a6e6758003a2531c326e8a1
Nikolai Artemiev <26139730+nikolaiartemiev@users.noreply.github.com>
2018-01-22 23:22:11 +1300

    Add rendering code

--- render.js
+++ render.js
@@ -1,5 +1,43 @@
-const canvas = document.getElementById("render-output");
-const cxt = canvas.getContext("2d");
+class Renderer {
+    constructor() {
+        this.canvas = document.getElementById("render-output");
+        this.cxt = this.canvas.getContext("2d");
 
-cxt.fillStyle = "#F0F";
-cxt.fillRect(0, 0, 10, 10);
+        window.addEventListener("resize", () => this.setCanvasSize());
+        this.setCanvasSize();
+    }
+
+    setCanvasSize() {
+        this.canvas.width  = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+    }
+
+    drawLine(sx, sy, ex, ey, color, thickness = 2) {
+        const cxt = this.cxt;
+        cxt.strokeStyle = color;
+        cxt.lineWidth = thickness;
+        
+        cxt.beginPath();
+        cxt.moveTo(sx, sy);
+        cxt.lineTo(ex, ey);
+        cxt.stroke();
+    }
+
+    drawPolygon(points, color) {
+        const cxt = this.cxt;
+        cxt.fillStyle = color;
+
+        cxt.beginPath();
+        cxt.moveTo(points[0][0], points[0][1]);
+        for(const point of points) {
+            cxt.lineTo(point[0], point[1]);
+        }
+        cxt.closePath();
+        cxt.fill();
+    }
+}
+
+
+const renderer = new Renderer();
+renderer.drawLine(0, 0, 100, 100, "red");
+renderer.drawPolygon([[200, 200], [200, 300], [300, 200]], "green");
